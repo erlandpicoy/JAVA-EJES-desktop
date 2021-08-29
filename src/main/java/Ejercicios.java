0dc4b81b9264fe92a2f299f8d29c38f7a4d6621f
ivanpicoy <picoy.ivan@gmail.com>
2021-08-29 02:31:09 -0300

    Ejercicio_Funciones_1
    crear una funcion que nos devuelva la factorial de un numero entero.

---
+++ src/main/java/Ejercicios.java
@@ -0,0 +1,24 @@
+//Ejercitacion N1 crear una funcion que nos devuelva la factorial de un numero entero.
+
+public class Ejercicios {
+
+    public static void main(String []args){
+        System.out.println(factorial(30));
+    }
+
+    public static int factorial(int numero){
+        if (numero <0){
+            System.out.println("Error 404");
+            return -1;
+        }
+        if (numero==0 && numero==1){
+            return 1;
+        }
+        int resultado = numero;
+
+        for(int i=(numero-1);i>1;i--){
+            resultado= resultado * i;
+        }
+        return resultado;
+    }
+}
