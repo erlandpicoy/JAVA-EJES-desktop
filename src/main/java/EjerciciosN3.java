1ceb3d0540838f229c1cf73be783d67a6401e932
ivanpicoy <picoy.ivan@gmail.com>
2021-09-02 00:36:15 -0300

    EjerciciosN3
    Ejercicio de operadores y secuenciales

--- src/main/java/EjerciciosN3.java
+++ src/main/java/EjerciciosN3.java
@@ -1,4 +1,31 @@
+//ingresar dado un tiempo en minutos por teclado, calcular los días, horas y minutos que le corresponden.
+import java.util.*;
 public class EjerciciosN3 {
-sout
+    public static void main (String []args){
+        calculoFecha(ingreseTeclado());
+    }
 
+    public static int ingreseTeclado(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese un valor:");
+        int teclado=sc.nextInt();
+        return teclado;
+    }
+
+    public static void calculoFecha(int valor){
+        int dias,horas, minutos;
+        int minutos_dia = 1440;
+        int resto_horas,resto_minutos;
+
+        dias=valor/minutos_dia;
+        resto_horas=valor-(minutos_dia*(valor/minutos_dia));
+        horas=resto_horas / 60;
+        resto_minutos= resto_horas-(60*(60/resto_horas));
+        minutos = resto_minutos - (60*(resto_minutos/60));
+
+        System.out.println("Resultado:" + dias + " dias");
+        System.out.println("Resultado " + horas + " horas");
+        System.out.println("Resultado " + minutos + " minutos");
+
+    }
 }
