124a02af44be56e68ee749e4d0ce1e7e9127f876
ivanpicoy <picoy.ivan@gmail.com>
2021-09-28 00:08:32 -0300

    Vectores_1

---
+++ src/main/java/EjerciciosN14.java
@@ -0,0 +1,11 @@
+public class EjerciciosN14 {
+    public static void main (String []args){
+        int numero[]=new int[5];
+
+        for (int i=0;i<5;i++){
+            //System.out.println(numero[i]);
+            numero[i]=i+1;
+            System.out.println(numero[i]);
+        }
+    }
+}
