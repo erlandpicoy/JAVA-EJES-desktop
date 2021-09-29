821863815d087aaa9a6395bc565828998ceee237
ivanpicoy <picoy.ivan@gmail.com>
2021-09-29 00:09:17 -0300

    EjercicioN15_Array Bidimensionales

---
+++ src/main/java/EjerciciosN15.java
@@ -0,0 +1,19 @@
+public class EjerciciosN15 {
+    public static void main (String []args){
+
+        String nombre [][]=new String[2][3];
+
+        nombre[0][0]="Ivan";
+        nombre[0][1]="Juan";
+        nombre[1][0]="Alberto";
+        nombre[1][1]="Tomas";
+
+        System.out.print(nombre[0][0]);
+        System.out.print(nombre[0][1]);
+        System.out.println("");
+        System.out.print(nombre[1][0]);
+        System.out.print(nombre[1][1]);
+
+    }
+
+}
